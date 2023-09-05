32fc7201ac4713193749e0a1fb345fa80d5c6272
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-09-05 10:13:52 +0700

    Add Start Game Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
+import React, { useState } from "react";
 import backgroundVid from "../public/background-vid.mp4";
+import StartGame from "./components/StartGame";
 
 function App() {
+  const [startGame, setStartGame] = useState(false);
   return (
-    <>
+    <React.Fragment>
+      {startGame ? <h2>Hello</h2> : <StartGame setStartGame={setStartGame} />}
       <video playsInline autoPlay muted loop poster="naruto.jpg">
         <source src={backgroundVid} type="video/mp4" />
       </video>
-    </>
+    </React.Fragment>
   );
 }
 
